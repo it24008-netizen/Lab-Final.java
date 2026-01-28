56e19c37e3aab8f539b95262a6d3270388bb2016
it24008-netizen <it24008@mbstu.ac.bd>
2026-01-28 15:21:57 +0600

    Inheritance & Method Overriding.java

--- Inheritance & Method Overriding.java
+++ Inheritance & Method Overriding.java
@@ -1,43 +1,26 @@
 class Employee {
     public double calculateSalary() {
-        return 0.0;
+        return 0;
     }
 }
 
 class FullTimeEmployee extends Employee {
-    private double basicSalary;
-    
-    public FullTimeEmployee(double basicSalary) {
-        this.basicSalary = basicSalary;
-    }
-
-    @Override
     public double calculateSalary() {
-        return basicSalary + 5000; 
+        return 50000;
     }
 }
 
 class PartTimeEmployee extends Employee {
-    private double hourlyRate;
-    private int hoursWorked;
-
-    public PartTimeEmployee(double hourlyRate, int hoursWorked) {
-        this.hourlyRate = hourlyRate;
-        this.hoursWorked = hoursWorked;
-    }
-
-    @Override
     public double calculateSalary() {
-        return hourlyRate * hoursWorked;
+        return 20000;
     }
 }
 
-public class EmployeeDemo {
+public class TestEmployee {
     public static void main(String[] args) {
-        Employee e1 = new FullTimeEmployee(20000);
-        Employee e2 = new PartTimeEmployee(200, 80);
-
-        System.out.println("Full Time Employee Salary: " + e1.calculateSalary());
-        System.out.println("Part Time Employee Salary: " + e2.calculateSalary());
+        Employee e1 = new FullTimeEmployee();
+        Employee e2 = new PartTimeEmployee();
+        System.out.println(e1.calculateSalary());
+        System.out.println(e2.calculateSalary());
     }
 }
