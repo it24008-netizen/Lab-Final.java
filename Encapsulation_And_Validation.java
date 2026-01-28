92355d25ed6ca2cbbe48ae17180613574b789d4e
it24008-netizen <it24008@mbstu.ac.bd>
2026-01-28 15:18:32 +0600

    Encapsulation_And_Validation.java

--- Encapsulation_And_Validation.java
+++ Encapsulation_And_Validation.java
@@ -1,60 +1,45 @@
-import java.util.Scanner;
 
 class Student {
-   
     private int id;
     private String name;
     private double cgpa;
 
-   
-    public Student(int id, String name, double cgpa) {
-        this.id = id;
-        this.name = name;
-        setCgpa(cgpa); 
-    }
-
-
     public int getId() {
         return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 
     public String getName() {
         return name;
     }
 
-    public double getCgpa() {
-        return cgpa;
-    }
-
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
     public void setName(String name) {
         this.name = name;
+    }
+
+    public double getCgpa() {
+        return cgpa;
     }
 
     public void setCgpa(double cgpa) {
         if (cgpa >= 0.0 && cgpa <= 4.0) {
             this.cgpa = cgpa;
         } else {
-            System.out.println("Invalid CGPA! Must be between 0.0 and 4.0. Setting CGPA to 0.0");
-            this.cgpa = 0.0;
+            System.out.println("Invalid CGPA");
         }
-    }
-
-    public void display() {
-        System.out.println("ID: " + id + ", Name: " + name + ", CGPA: " + cgpa);
     }
 }
 
-public class StudentDemo {
+public class Main1 {
     public static void main(String[] args) {
-        Student s1 = new Student(101, "Apurbo", 3.8);
-        Student s2 = new Student(102, "Rahim", 4.5); 
-
-        s1.display();
-        s2.display();
+        Student s = new Student();
+        s.setId(1);
+        s.setName("Alice");
+        s.setCgpa(3.6);
+        System.out.println(s.getCgpa());
+        s.setCgpa(4.8);
     }
 }
